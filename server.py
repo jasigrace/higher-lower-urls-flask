6397773ed4b5ca7c8c7e250324d7cef3a0127cac
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-05-16 07:10:17 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,32 @@
+import random
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def guess_number():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://i.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.webp">'
+
+
+random_number = random.randint(0, 9)
+
+
+@app.route('/<int:number>')
+def detect_number(number):
+    print(random_number)
+    if number == random_number:
+        return '<h1 style="color:green">You found me!</h1>' \
+               '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
+    elif number < random_number:
+        return '<h1 style="color:red">Too low, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
+    elif number > random_number:
+        return '<h1 style="color:purple">Too high, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
